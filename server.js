8ebe9324442ccc868e717107f169cb471f8d7ac7
Seng Phank <seng.phank173@gmail.com>
2025-09-26 17:17:33 +0930

    t

--- server.js
+++ server.js
@@ -1,33 +1,68 @@
 const express = require('express');
-const { createServer } = require('node:http');
-const { join } = require('node:path');
-const { Server } = require('socket.io');
+const { v4: uuidv4 } = require('uuid');
+const WebSocket = require('ws'); // Websocket for sessionID
+const { spawn } = require('child_process'); // For running external files like scraper.py
 
-// Global variable userIDs ()
-var IDs = 0; // Current userID
+const app = express();
+const PORT = 3000;
 
-const app = express(); // Function Handler  
-const server = createServer(app);
-const io = new Server(server, { // Create listener for incoming sockets
-  connectionStateRecovery: {} // Attempt auto-recover on disconnect
-}); 
-
-app.get('/', (req, res) => { // On initialised (main)
-  res.sendFile(join(__dirname, 'public/index.html')); // send public html
+const server = app.listen(PORT, () => {
+  console.log(`Server running on http://localhost:${PORT}`);
 });
-// Change root
-app.use(express.static(join(__dirname, 'public')));
 
-io.on('connection', (socket) => {
-  console.log('a user connected:', socket.id);
+const wss = new WebSocket.Server({ server });
 
-  socket.on('chat message', (data) => {
-    console.log(`message from ${data.userId}: ${data.message}`);
-    io.emit('chat message', data); // broadcast to all clients
+// Store rooms and users in memory
+const rooms = {}; // { roomId: { users: [socket1, socket2, ...] } }
+
+// Handle new connections
+wss.on('connection', (socket) => {
+  socket.on('message', (data) => {
+    const msg = JSON.parse(data);
+
+    if (msg.type === 'create_room') {
+      const roomId = uuidv4();
+      rooms[roomId] = { users: [socket] };
+      socket.roomId = roomId;
+      socket.send(JSON.stringify({ type: 'room_created', roomId }));
+    }
+
+    if (msg.type === 'join_room') {
+      const roomId = msg.roomId;
+      if (rooms[roomId]) {
+        rooms[roomId].users.push(socket);
+        socket.roomId = roomId;
+        socket.send(JSON.stringify({ type: 'room_joined', roomId }));
+        // Notify others
+        rooms[roomId].users.forEach(user => {
+          if (user !== socket) {
+            user.send(JSON.stringify({ type: 'user_joined', msg: 'A new user joined!' }));
+          }
+        });
+      } else {
+        socket.send(JSON.stringify({ type: 'error', message: 'Room not found' }));
+      }
+    }
+
+    if (msg.type === 'message') {
+      const roomId = socket.roomId;
+      if (rooms[roomId]) {
+        rooms[roomId].users.forEach(user => {
+          if (user !== socket) {
+            user.send(JSON.stringify({ type: 'message', text: msg.text }));
+          }
+        });
+      }
+    }
+  });
+
+  socket.on('close', () => {
+    const roomId = socket.roomId;
+    if (roomId && rooms[roomId]) {
+      rooms[roomId].users = rooms[roomId].users.filter(u => u !== socket);
+      if (rooms[roomId].users.length === 0) {
+        delete rooms[roomId]; // clean up
+      }
+    }
   });
 });
-
-
-server.listen(3000, () => { // Listen on port 3000
-  console.log('server running at http://localhost:3000');
-}); 
